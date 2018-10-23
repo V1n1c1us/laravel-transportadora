45b57a8797629db0ce0dbcda568c3fbd12526755
Vinícius <v.f.diehl@gmail.com>
2018-10-23 17:24:36 -0300

    Route Adjustments

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,25 @@
 |
 */
 
-$this->get('/','HomeController@index')->name('home');
+Route::get('/',['as' => 'home','uses' => 'HomeController@index']);
 
-$this->get('produto', 'ProdutoController@index')->name('produto.index');
+Route::get('/produto/create',['as' => 'produto.create','uses' => 'ProdutoController@index']);
+Route::get('/produto/edit/{id}',['as' => 'produto.edit','uses' => 'ProdutoController@edit']);
+Route::get('/produto/view/{id}',['as' => 'produto.view','uses' => 'ProdutoController@view']);
+Route::post('/produto/store',['as' => 'produto.store','uses' => 'ProdutoController@store']);
+Route::post('/produto/update',['as' => 'produto.update','uses' => 'ProdutoController@update']);
+Route::get('/produto/delete/{id}', ['as' => 'produto.delete','uses'=>'ProdutoController@delete']);
+
+
+Route::get('/fornecedor/create',['as' => 'fornecedor.create','uses' => 'FornecedorController@index']);
+
+
+//$this->get('produto', 'ProdutoController@index')->name('produto.index');
 $this->get('produto/info/{id}', 'ProdutoController@getInfo');
 $this->post('produto.store', 'ProdutoController@store')->name('produto.store');
-$this->get('produto/delete/{id}', 'ProdutoController@delete');
-$this->get('produto/edit/{id}', 'ProdutoController@edit');
+//Route::get('produto/delete/{id}', 'ProdutoController@delete');
+//$this->get('produto/edit/{id}', 'ProdutoController@edit');
 $this->post('produto/update/{id}', 'ProdutoController@update')->name('produto.update');
 
-$this->get('fornecedor', 'FornecedorController@index')->name('fornecedor.index');
+//$this->get('fornecedor', 'FornecedorController@index')->name('fornecedor.index');
 $this->post('store', 'FornecedorController@store')->name('fornecedor.store');
